75e0241082822a30792cbd4e51823db27fad2e11
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-13 08:49:04 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer;
+const leastFavoriteCustomer = 'hello';
+
+function upperCaseCustomerName()
+{
+    let newName = customerName.toUpperCase()
+    return newName
+    // customerName.toUpperCase()
+    
+}
+
+
+
+function setBestCustomer()
+{
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer()
+{
+    return bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer()
+{
+    return leastFavoriteCustomer = 'Assignment to constant variable.'
+
+}
